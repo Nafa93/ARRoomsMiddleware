5bb615c308a7fb0169f8cd052b60a98e0b9b5a2f
Nicolas Fernandez Amorosino <>
2018-01-10 12:09:02 -0300

    rolled back modularization added start script

--- server/server.js
+++ server/server.js
@@ -1,6 +1,41 @@
+
 const express = require('express')
-const google = require('googleapis')
-const {getAccessToken, oauth2Client, url} = require('../google/authentication')
+var google = require('googleapis')
+var OAuth2 = google.auth.OAuth2
+var readline = require('readline')
+
+var clientSecret = 'A0JzbPxlHFJhdJ069IQgbcIM'
+var clientId = '153639822975-ik5n1tlshg91mvuo4lqcuhhkm18asqn7.apps.googleusercontent.com'
+var redirectUrl = 'http://localhost:8081'
+
+var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl)
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function getAccessToken (oauth2Client, callback) {
+  // generate consent page url
+  var url = oauth2Client.generateAuthUrl({
+    // access_type: 'online', // will return a refresh token
+    scope: 'https://www.googleapis.com/auth/calendar' // can be a space-delimited string or an array of scopes
+  })
+
+  console.log('Visit the url: ', url)
+  rl.question('Enter the code here:', function (code) {
+    // request access token
+    oauth2Client.getToken(code, function (err, tokens) {
+      if (err) {
+        return callback(err)
+      }
+      // set tokens to the client
+      // TODO: tokens should be set by OAuth2 client.
+      oauth2Client.credentials = tokens
+      callback()
+    })
+  })
+}
 
 var calendar = google.calendar({version: 'v3'})
 
@@ -18,10 +53,6 @@ app.get('/test', (req, res) => {
       console.log(response)
     })
   })
-})
-
-app.get('/authUrl', (req, res) => {
-  res.send(url)
 })
 
 app.listen(port, () => {
