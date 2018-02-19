ff7cfc7d2e8bfba2692320de6b389284af9a820b
Nicolas Fernandez Amorosino <>
2018-02-19 14:57:20 -0300

    addded log to debug env var

--- server/server.js
+++ server/server.js
@@ -3,54 +3,55 @@ const express = require('express')
 const google = require('googleapis')
 const moment = require('moment')
 const calendar = google.calendar({version: 'v3'})
-var key = require('../jwt.keys.json')
+// var key = require('../jwt.keys.json')
 var timeMin = moment().hour(0).minute(0).second(0).format()
 var timeMax = moment().hour(23).minute(59).second(59).format()
 
-var jwtClient = new google.auth.JWT(
-  key.client_email,
-  null,
-  key.private_key,
-  ['https://www.googleapis.com/auth/calendar'], // an array of auth scopes
-  null
-)
+// var jwtClient = new google.auth.JWT(
+//   key.client_email,
+//   null,
+//   key.private_key,
+//   ['https://www.googleapis.com/auth/calendar'], // an array of auth scopes
+//   null
+// )
 
 var app = express()
 
 var port = 3001
 
 app.get('/events/today/:calendarId', (req, res) => {
-  var calendarId = req.params.calendarId
+  res.status(200).send(process.env)
+  // var calendarId = req.params.calendarId
 
-  jwtClient.authorize(function (err, tokens) {
-    if (err) {
-      console.log(err)
-    } else {
-      calendar.events.list(
-        { calendarId,
-          auth: jwtClient,
-          timeMin,
-          timeMax,
-          singleEvents: true
-        },
-        function (err, response) {
-          var customResponse = {
-            items: response.items ? response.items : null,
-            message: '',
-            err
-          }
-          if (err) {
-            res.status(400).send(customResponse)
-          } else if (response.items.length > 0) {
-            customResponse.message = `There are ${response.items.length} events in this room`
-            res.status(200).send(customResponse)
-          } else {
-            customResponse.message = 'There are no events today'
-            res.status(200).send(customResponse)
-          }
-        })
-    }
-  })
+  // jwtClient.authorize(function (err, tokens) {
+  //   if (err) {
+  //     console.log(err)
+  //   } else {
+  //     calendar.events.list(
+  //       { calendarId,
+  //         auth: jwtClient,
+  //         timeMin,
+  //         timeMax,
+  //         singleEvents: true
+  //       },
+  //       function (err, response) {
+  //         var customResponse = {
+  //           items: response.items ? response.items : null,
+  //           message: '',
+  //           err
+  //         }
+  //         if (err) {
+  //           res.status(400).send(customResponse)
+  //         } else if (response.items.length > 0) {
+  //           customResponse.message = `There are ${response.items.length} events in this room`
+  //           res.status(200).send(customResponse)
+  //         } else {
+  //           customResponse.message = 'There are no events today'
+  //           res.status(200).send(customResponse)
+  //         }
+  //       })
+  //   }
+  // })
 })
 
 app.listen(port, () => {
