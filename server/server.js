752d656a2d5031b22f0c21f13a21dc30c897dbe5
Nicolas Fernandez Amorosino <>
2018-02-19 16:13:02 -0300

    debug for jwt

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const express = require('express')
 const google = require('googleapis')
 const moment = require('moment')
 const calendar = google.calendar({version: 'v3'})
-// var key = require('../jwt.keys.json')
+var key = require('../jwt.keys.json')
 var timeMin = moment().hour(0).minute(0).second(0).format()
 var timeMax = moment().hour(23).minute(59).second(59).format()
+
+process.env.client_email = key.client_email
+process.env.private_key = key.private_key
 
 var jwtClient = new google.auth.JWT(
   process.env.client_email,
@@ -22,7 +25,8 @@ var port = process.env.PORT || 3001
 app.get('/env', (req, res) => {
   res.send({
     'response': process.env,
-    'message': 'success'
+    'message': 'success',
+    jwtClient
   })
 })
 
